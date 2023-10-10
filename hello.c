8156f5fa029cb797aab6467d165e2b6c8ec205f4
Joe <Joe@aux.com>
2023-10-10 20:15:43 +0800

    second commit

--- hello.c
+++ hello.c
@@ -8,5 +8,10 @@ int main()
     printf("hello,world!");
     printf("hello,world!");
     printf("hello,world!");
+    printf("something is added at present.");
+    printf("something is added at present.");
+    printf("something is added at present.");
+    printf("something is added at present.");
+    printf("something is added at present.");
     return 0;
 }
